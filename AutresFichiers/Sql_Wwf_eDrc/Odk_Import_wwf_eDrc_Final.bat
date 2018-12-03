d24098f8a2781033c8bc94d119a0df47b21272de
michelo243 <misamuna@gmail.com>
2018-12-03 11:09:25 +0200

    update code sql for the project

---
+++ AutresFichiers/Sql_Wwf_eDrc/Odk_Import_wwf_eDrc_Final.bat
@@ -0,0 +1,4 @@
+java -jar D:\ODK\ODKBriefcaseProduction.jar -id fiche_terrain_reboiser_v5 -sd D:\ODK -url https://wwfdrc-02.appspot.com -u bienfait -p @juillet2017
+java -jar D:\ODK\ODKBriefcaseProduction.jar -id fiche_identification_pepiniere_v6 -sd D:\ODK -url https://wwfdrc-02.appspot.com -u bienfait -p @juillet2017
+java -jar D:\ODK\ODKBriefcaseProduction.jar -id fiche_suivi_pepiniere_v5 -sd D:\ODK -url https://wwfdrc-02.appspot.com -u bienfait -p @juillet2017
+exit
--- AutresFichiers/Sql_Wwf_eDrc/SQL_Final_xEntryGlobal_Db_StoreProc.sql
+++ AutresFichiers/Sql_Wwf_eDrc/SQL_Final_xEntryGlobal_Db_StoreProc.sql
@@ -62,3 +62,15 @@ as
 Exec xp_cmdshell 'java -jar D:\ODK\ODKBriefcaseProduction.jar -id fiche_suivi_pepiniere_v5 -sd D:\ODK -url https://wwfdrc-02.appspot.com -u bienfait -p @juillet2017'
 Exec xp_cmdshell 'D:\Release\xEntryFmpOdkXmlImporter.exe -sWWF_SERVER12\SQLSERVWWFE18  -dxEntryGlobalDb -uwwfadmin -proot//777Wwf -f"D:\ODK\ODK Briefcase Storage\forms\Fiche de suivi pépinière (v5)\instances" -g4'
 GO
+
+
+-- 1. TAR
+Exec xp_cmdshell 'java -jar D:\ODK\ODKBriefcaseProduction.jar -id fiche_terrain_reboiser_v5 -sd D:\ODK -url https://wwfdrc-02.appspot.com -u bienfait -p @juillet2017'
+Exec xp_cmdshell 'D:\ODK\Release\xEntryFmpOdkXmlImporter.exe -sWWF_SERVER12\SQLSERVWWFE18  -dxEntryGlobalDb -uwwfadmin -proot//777Wwf -f"D:\ODK\ODK Briefcase Storage\forms\Fiche de terrain à reboiser (v5)\instances" -g1'
+--2. IDENTIF PEPI
+Exec xp_cmdshell 'java -jar D:\ODK\ODKBriefcaseProduction.jar -id fiche_identification_pepiniere_v6 -sd D:\ODK -url https://wwfdrc-02.appspot.com -u bienfait -p @juillet2017'
+Exec xp_cmdshell 'D:\ODK\Release\xEntryFmpOdkXmlImporter.exe -sWWF_SERVER12\SQLSERVWWFE18  -dxEntryGlobalDb -uwwfadmin -proot//777Wwf -f"D:\ODK\ODK Briefcase Storage\forms\Fiche identification pepiniere (v6)\instances" -g3'
+--3. SUIVI PEPI
+Exec xp_cmdshell 'java -jar D:\ODK\ODKBriefcaseProduction.jar -id fiche_suivi_pepiniere_v5 -sd D:\ODK -url https://wwfdrc-02.appspot.com -u bienfait -p @juillet2017'
+Exec xp_cmdshell 'D:\ODK\Release\xEntryFmpOdkXmlImporter.exe -sWWF_SERVER12\SQLSERVWWFE18  -dxEntryGlobalDb -uwwfadmin -proot//777Wwf -f"D:\ODK\ODK Briefcase Storage\forms\Fiche de suivi pépinière (v5)\instances" -g4'
+
